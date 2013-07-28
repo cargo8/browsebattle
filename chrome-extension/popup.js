b3502a061326609b089f64917d0dd1e7c4a64122
yunxing <yunxing@upthere.com>
2013-07-28 11:05:51 -0700

    new popup page

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,7 +1,25 @@
+function rankToLevel(rank) {
+    if (rank > 100) {
+        rank = 1;
+    } else {
+        rank = 100 - rank;
+    }
+    return rank;
+}
+
+function urlClean(url) {
+    url = url.replace("http://", "");
+    url = url.replace("https://", "");
+    url = url.replace("www.", "");
+    url = url.replace("/", "");
+    return url;
+}
+
 $(document).ready(function(){
     chrome.storage.local.get('player', function(data) {
         if (!data.player) {
             chrome.tabs.query({active:true},function(tab){
+                $("#pic").hide();
                 $("#url").val(tab[0].url);
                 $("#btn").text("Start Battle");
                 $("#btn").click(function(){
@@ -10,6 +28,13 @@ $(document).ready(function(){
                 });
             });
         } else {
+            $("#url").hide();
+            var name = urlClean(data.player);
+            $("#pic").attr("src", "http://"+name+"/favicon.ico");
+            get_rank(data.player, function(rank){
+                var level = rankToLevel(rank);
+                $("#level").html("Current Level:" + level);
+            });
             $("#url").val(data.player);
             $("#btn").text("Stop Battle");
             $("#btn").click(function(){
