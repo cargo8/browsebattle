8cfb225b98b9409ec03c9a2889c9b7299739988a
yunxing <yunxing@upthere.com>
2013-07-27 22:30:10 -0700

    popup menu

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-    chrome.storage.local.set({'initurl': "hiya"}, function() {
-        chrome.storage.local.get('initurl', function(data) {
-            $("#btn").text(data.initurl);
-        });
+    chrome.storage.local.get('player', function(data) {
+        if (!data.player) {
+            $("#btn").text("Start Battle");
+            $("#btn").click(function(){
+                chrome.storage.local.set({'player': $("#url").val()}, function(data){
+                    chrome.tabs.update({ active: true });
+                });
+
+            });
+        } else {
+            $("#url").val(data.player);
+            $("#btn").text("Stop Battle");
+            $("#btn").click(function(){
+                chrome.storage.local.remove('player', function(data){
+                    chrome.tabs.update({ active: true });
+                });
+            });
+        }
     });
+
+    // chrome.storage.local.set({'initurl': "hiya"}, function() {
+
+    //         $("#btn").text(data.initurl);
+    //     });
 });
